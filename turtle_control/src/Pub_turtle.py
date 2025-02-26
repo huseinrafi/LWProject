17b2147d41902436e50d7f7d2bb4ef729ac931c5
huseinrafi <164824092+huseinrafi@users.noreply.github.com>
2025-02-26 16:38:21 +0700

    Update Pub_turtle.py
    edit hapus pemborosan kode

--- turtle_control/src/Pub_turtle.py
+++ turtle_control/src/Pub_turtle.py
@@ -5,15 +5,11 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher('/chatter', String, queue_size=10)  # Membuat publisher ke topic 'chatter'
     rospy.init_node('talker', anonymous=True)  # Inisialisasi node dengan nama 'talker'
-    rate = rospy.Rate(1)  # Frekuensi pengiriman pesan (1 Hz = 1 detik sekali)
 
     while not rospy.is_shutdown():
         input_message = input("Masukkan Perintah untuk menggerakkan Turtle (Maju, Mundur, Kiri, Kanan)")  # Meminta input pesan dari user  
-        message = String()  # Membuat objek message dengan tipe data String
-        message.data = input_message # Mengisi data message dengan input dari user
-        rospy.loginfo(f"Sending: {message.data}")
-        pub.publish(message.data)
-        rate.sleep()  # Tunggu sesuai rate
+        rospy.loginfo(f"Sending: {input_message}")
+        pub.publish(input_message)
 
 if __name__ == "__main__":
     try:
